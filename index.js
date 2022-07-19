39e5ba41b29839e9f1129e78669db6d0befe65c7
+ <+>
2022-07-19 12:15:57 -0400

    commit tree c3b6636c66fd9e1552b7c8578b1f8b42e6e062d7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(string) {
+    if (string) {
+        return `This Saturday, I want to ${string}!`
+    } else {
+   return 'This Saturday, I want to roller-skate!' 
+    }
+}
+
+function mondayWork(string) {
+    if (string) {
+        return `This Monday, I will ${string}.`
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+function wrapAdjective(string = '*') {
+    return function(special) {
+        return `You are ${string}${special}${string}!`
+    }
+}
